81292cb1bbabc566d1d3475e5496f0831c82d833
Danilo Acosta <danilo.acostadote@chasacademy.se>
2024-07-09 02:27:03 +0200

    BlockUser & UpgradeUserRole

--- Backend/WisApi/WisApi/Controllers/UserControllers/UserController.cs
+++ Backend/WisApi/WisApi/Controllers/UserControllers/UserController.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Azure.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 using WisApi.Migrations.ApplicationDb;
 using WisApi.Models;
 using WisApi.Models.DTO_s.ProfileDTOs;
@@ -14,14 +18,16 @@ namespace WisApi.Controllers.UserControllers
     {
         private readonly UserManager<ExtendedIdentityUser> _userManager;
         private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public UserController(UserManager<ExtendedIdentityUser> userManager, IWebHostEnvironment webHostEnvironment)
+        public UserController(UserManager<ExtendedIdentityUser> userManager, IWebHostEnvironment webHostEnvironment, IHttpContextAccessor httpContextAccessor)
         {
             _userManager = userManager;
             _webHostEnvironment = webHostEnvironment;
-
+            _httpContextAccessor = httpContextAccessor;
         }
 
+        //Change GetUser to retrieve data from UserRepository instead of 
         [HttpGet("{userName}")]
         public ActionResult<GetProfileDTO> GetUser(string userName) 
         {
@@ -42,7 +48,7 @@ namespace WisApi.Controllers.UserControllers
             return Ok(result);
         }
 
-
+        //The following action updates users Bio and Image
         [HttpPut]
         public async Task<IActionResult> UpdateUserDetails([FromForm] EditProfileDTO model)
         {
@@ -63,6 +69,7 @@ namespace WisApi.Controllers.UserControllers
             return Ok();
         }
 
+        //SaveImage should be in a service
         [NonAction]
         public async Task<string> SaveImage(IFormFile imageFile)
         {
@@ -75,5 +82,95 @@ namespace WisApi.Controllers.UserControllers
             }
             return imageName;
         }
+
+
+        //Don't forget to set authorized roles here
+        //Blocking action 
+        [HttpPut("block")]
+        [Authorize(Roles = "Admin, Super")]
+        public async Task<ActionResult> BlockUser([FromBody]string userName)
+        {
+            var user = await _userManager.FindByNameAsync(userName);
+            if (user.UserName == userName)
+            {
+                user.IsBlocked = true;
+                await _userManager!.UpdateAsync(user);
+                return Ok("User blocked");
+            }
+            return BadRequest();
+        }
+
+        //Don't forget to set authorized roles here
+        //Unblocking user
+        [HttpPut("unblock")]
+        [Authorize(Roles = "Admin, Super")]
+        public async Task<ActionResult> UnBlockUser([FromBody] string userName)
+        {
+            var user = await _userManager.FindByNameAsync(userName);
+            if (user.UserName == userName)
+            {
+                user.IsBlocked = false;
+                await _userManager!.UpdateAsync(user);
+                return Ok("User unblocked");
+            }
+            return BadRequest();
+        }
+
+        //UpgradeUserRole 
+        [HttpPut("upgrade/user")]
+        [Authorize(Roles = "Creator, Admin, Super")]
+        public async Task<ActionResult> UpgradeUserRole([FromBody] UpgradeRoleDTO upgradeInfo)
+        {
+
+            //Make This Action less fat
+            //Use repository/Services
+
+            var upgradedByUser = _httpContextAccessor.HttpContext.User;
+            var byUserName = upgradedByUser.FindFirstValue(ClaimTypes.Name);
+            var byRole = upgradedByUser.FindFirstValue(ClaimTypes.Role);
+
+            //Lookup target user, if not found, return NotFound()
+            var user = await _userManager!.FindByNameAsync(upgradeInfo.TargetUser);
+            if (user.UserName == upgradeInfo.TargetUser)
+            {
+                var currentRoleList = await _userManager.GetRolesAsync(user);
+                var currentRole = currentRoleList.SingleOrDefault();
+
+                if (byRole == "Creator") {
+
+                    if (currentRole != "User") 
+                        return BadRequest($"{upgradeInfo.TargetUser} is not upgradable by {byUserName}");
+
+                    await _userManager.AddToRoleAsync(user, upgradeInfo.NewRole);
+                    await _userManager.RemoveFromRoleAsync(user, currentRole);
+                    return Ok($"Successfully changed {user.UserName}'s role from {currentRole} to {upgradeInfo.NewRole}");
+                }
+
+                if (byRole == "Admin")
+                {
+                    if (currentRole != "User" || currentRole != "Creator") 
+                        return BadRequest($"{upgradeInfo.TargetUser} is not upgradable by {byUserName}");
+
+                    await _userManager.AddToRoleAsync(user, upgradeInfo.NewRole);
+                    await _userManager.RemoveFromRoleAsync(user, currentRole);
+                    return Ok($"Successfully changed {user.UserName}'s role from {currentRole} to {upgradeInfo.NewRole}");
+
+                }
+
+                if (byRole == "Super")
+                {
+                    //What if it is another super user?
+
+                    await _userManager.AddToRoleAsync(user, upgradeInfo.NewRole);
+                    await _userManager.RemoveFromRoleAsync(user, currentRole);
+                    return Ok($"Successfully changed {user.UserName}'s role from {currentRole} to {upgradeInfo.NewRole}");
+                }
+                return BadRequest("Something went wrong. Please try again.");
+            }
+            return NotFound();
+        }
+
+
+        //Report user action
     }
 }
---
+++ Backend/WisApi/WisApi/Models/DTO's/ProfileDTOs/UpgradeRoleDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace WisApi.Models.DTO_s.ProfileDTOs
+{
+    public class UpgradeRoleDTO
+    {
+        public string TargetUser { get; set; }
+
+        public string NewRole { get; set; }
+    }
+}
