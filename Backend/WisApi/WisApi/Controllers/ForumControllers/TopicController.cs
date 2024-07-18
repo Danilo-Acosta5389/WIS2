39fb8cb808471e3ce74f2c7feef1e504854f6e69
Danilo Acosta <danilo.acostadote@chasacademy.se>
2024-07-18 15:03:55 +0200

    test

--- Backend/WisApi/WisApi/Controllers/ForumControllers/TopicController.cs
+++ Backend/WisApi/WisApi/Controllers/ForumControllers/TopicController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Azure;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WisApi.Models;
+using WisApi.Models.DTO_s;
 using WisApi.Models.DTO_s.ForumDTOs;
 using WisApi.Repositories.Interfaces;
 
@@ -13,7 +15,7 @@ namespace WisApi.Controllers.ForumControllers
     {
         private readonly ITopicRepository _topicRepository;
         private readonly UserManager<ExtendedIdentityUser> _userManager;
-        public TopicController(ITopicRepository topicRepository, UserManager<ExtendedIdentityUser> userManager)
+        public TopicController(ITopicRepository topicRepository, UserManager<ExtendedIdentityUser> userManager) 
         {
             _topicRepository = topicRepository;
             _userManager = userManager;
@@ -45,7 +47,7 @@ namespace WisApi.Controllers.ForumControllers
 
         [HttpPost("Create")]
         [Authorize(Roles = "Creator, Admin, Super")]
-        public IActionResult CreateTopic(CreateTopicDTO topic)
+        public IActionResult CreateTopic(CreateTopicDTO topic) 
         {
             HttpContext.Request.Cookies.TryGetValue("publicId", out var publicId);
             HttpContext.Request.Cookies.TryGetValue("refreshToken", out var refreshToken);
@@ -68,7 +70,7 @@ namespace WisApi.Controllers.ForumControllers
                 Title = topic.Title,
                 Description = topic.Description,
                 CreatedAt = topic.CreatedAt,
-                UserName = topic.UserName,
+                UserName= topic.UserName,
                 UserId = user.Id,
                 IpAdress = ip,
                 IsAnonymous = topic.IsAnonymous
@@ -77,9 +79,10 @@ namespace WisApi.Controllers.ForumControllers
             _topicRepository.Create(newTopic);
             _topicRepository.Save();
 
-             
-            return Ok();
+            var responseMsg = new statusMessageDTO("Success!");
 
+            return Ok(responseMsg);
+            
         }
 
         [HttpPut("invisible")]
